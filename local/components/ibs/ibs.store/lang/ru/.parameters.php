e562256025f7ab69cf562dc49d6f395242162e46
neverspaik <neverspaik@gmail.com>
2025-05-24 23:14:10 +0300

    remove temp 24.05

--- local/components/ibs/ibs.store/lang/ru/.parameters.php
+++
@@ -1,4 +0,0 @@
-<?php
-$MESS["IBS_STORE_SEF_MODE"] = "Использовать ЧПУ (SEF)";
-$MESS["IBS_STORE_SEF_FOLDER"] = "Папка ЧПУ";
-$MESS["IBS_STORE_SEF_URL_TEMPLATES"] = "Шаблоны URL";
--- local/components/ibs/ibs.store/templates/.default/script.js
+++
@@ -1,7 +0,0 @@
-function rollback() {
-    if (window.history.length > 1) {
-        history.back();
-    } else {
-        window.location.href = '/';
-    }
-}
